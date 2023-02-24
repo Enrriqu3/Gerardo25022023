da42a900390e588c0cf7cc9489e5c40891893dfa
Enrriqu3 <125326292+Enrriqu3@users.noreply.github.com>
2023-02-23 18:50:22 -0600

    Semana #5 Asincrona

---
+++ Bucle_for (Asincrona - semana # 5)/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class Datos{
+    static void Main(string[] ArraySegment){
+        Console.WriteLine("Ingrese el primer numero");
+        int PrimerNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el ultimo numero");
+        int UltimoNum = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Los numeros pares entres {PrimerNum} y {UltimoNum} son:");
+        for (int i = PrimerNum; i <= UltimoNum ; i++){
+            if (i % 2 == 0){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
